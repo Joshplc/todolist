c28144537b1462c08f5bd604a4b7d3d40bd3378e
Josue <poncedeleonjosue@gmail.com>
2023-08-30 00:42:41 -0400

    I have already included the Controllers now, I have had to use ChatGPT to help me fix all the errors that have been anoying my code, it seems then that the Routers are the same Visual, then

--- routes/todosRouter.ts
+++ routes/todosRouter.ts
@@ -1,32 +1,9 @@
 import {Router} from 'express'
-import { validateToDo } from '../schemas/TodoSchemaValidation'
-import {Task, todoModel} from '../models/todoModel'
+ 
+import { todoController } from '../controllers/todoControllers'
 
 export const todosRouter = Router()
 
 
-todosRouter.get('/', async (req, res) => {
-    try {
-        const tasks: Task[] = await todoModel.getAllTasks()
-        res.json(tasks)
-    } catch (error) {
-        res.status(500).json({ error: 'Internal server error' })
-    }
-})
-
-todosRouter.post('/', async (req, res) => {
-    const result = validateToDo(req.body)
-    
-    if (result.error) {
-        return res.status(400).json({ error: JSON.parse(result.error.message )})
-    }
-
-    const newTask: Task = { id: Date.now(), ...req.body };
-
-    try {
-        await todoModel.addTask(newTask);
-        res.status(201).json(newTask);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
-    }
-})
+todosRouter.get('/', todoController.getAll)
+todosRouter.post('/', todoController.add)
